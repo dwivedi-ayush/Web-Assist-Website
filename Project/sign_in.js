3cac76f29cbc59e24d0bf39b01ffd2f2986c2227
ANIRUDH-VADERA <anirudhvadera@gmail.com>
2022-04-27 14:42:12 +0530

    Final Changes

--- Project/sign_in.js
+++
@@ -1,9 +0,0 @@
-function showPassword()
-{
-    var x = document.querySelector('.password');
-    if (x.type === "password") {
-      x.type = "text";
-    } else {
-      x.type = "password";
-    }
-}
